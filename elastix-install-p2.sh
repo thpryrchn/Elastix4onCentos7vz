0f825d5313720d2d7c2d3138b9d6eb70df5b3fce
Benjamin McGuire <jaminmc@gmail.com>
2016-03-24 13:34:41 -0400

    More KVM fixes

--- elastix-install-p2.sh
+++ elastix-install-p2.sh
@@ -9,10 +9,20 @@ echo "Now we are running some cleanup, and making sure everything is up to date"
 rm -rf /etc/yum.repos.d/elastix-cd.repo /mnt/iso/ Elastix-4.0.74-Stable-x86_64-bin-10Feb2016.iso
 mv /etc/yum.repos.d/elastix.repo.rpmnew /etc/yum.repos.d/elastix.repo
 yum clean all
+mkdir .poop
+cd .poop
+yum -r reinstall elastix*
+cd ..
+rm -rf .poop
 yum -y update
+echo " "
+echo " "
+echo " "
 echo " "
 echo "Note: The NetworkManager was not installed, as it breaks the Virtual Network"
 echo "interfaces that OpenVZ uses to provide Internet / Network access. " 
+echo " "
+echo " "
 echo ""
 echo "Now we need another reeboot to finish the setup."
 read -p "Press Enter to Reboot, or CTRL-C to abort."
